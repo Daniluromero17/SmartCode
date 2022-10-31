0ce023e5dcc536e4a09d2de483ace5be854cc5fe
Carlos F <103224983+F3nr1@users.noreply.github.com>
2022-10-30 19:33:58 -0500

    Version 1.0

---
+++ FrontEnd/MascotasApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    DashboardComponent,
+    PagesComponent,
+    ProductsComponent,
+    SettingsComponent,
+    SidenavComponent,
+    StatisticsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
